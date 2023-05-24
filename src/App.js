3aabfd2a15db3cf1ac0d61d9f1f060ccbe78a020
noffrizal <zaimjr@gmail.com>
2023-05-24 11:09:03 +0700

    tes

--- src/App.js
+++ src/App.js
@@ -5,18 +5,26 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 
+
 function App() {
+  const navText = 'Noffrizal Zaim'
+  const navHeading = 'Navigation Bar'
+
+  const clicked = () =>{
+    return alert('button dipencet')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <Navbar />
+        <Navbar test = {navText} navHeading = {navHeading} />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Halo Cuy selamat datang deh disini 
+          Halo Cuy selamat datang deh disini
         </p>
-        <MyButton />
+        <MyButton clicked={clicked}/>
         <Footer />
-       
+
       </header>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
-const Navbar = () => {
+const Navbar = (props) => {
     return(
         <div>
+            <h1>{props.navHeading}</h1>
         <ul>
             <li>
                 <a href="google.com">Home</a>
@@ -10,6 +11,9 @@ const Navbar = () => {
             </li>
             <li>
                 <a href="google.com">Contact</a>
+            </li>
+            <li>
+                <a href="google.com">{props.test}</a>
             </li>
         </ul>
     </div>
